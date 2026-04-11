83767e19642c42503ce53b8414a13a35ed4725d5
Luke Mendelman-Haenn <glukemh@gmail.com>
2026-04-10 19:07:58 -0500

    context node

--- index.js
+++ index.js
@@ -27,26 +27,74 @@ class Markup {
   }
 }
 
-/** @template {string} T */
+/**
+ * @template {string} T
+ * @template {{ [K in keyof M]: Element }} [M=HTMLElementTagNameMap]
+ */
 export class Template {
   static #range = document.createRange();
   static slotPrefix = /** @type {const} */ ("slot:");
+  static refAttr = /** @type {const} */ ("data-ref");
+
+  static #svgContext = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "svg",
+  );
+  static #mathMLContext = document.createElementNS(
+    "http://www.w3.org/1998/Math/MathML",
+    "math",
+  );
+
+  /**
+   * @template {string} T
+   * @param {T} template
+   */
+  static svg(template) {
+    return /** @type {Template<T, SVGElementTagNameMap>} */ (
+      new Template(template, Template.#svgContext)
+    );
+  }
+
+  /**
+   * @template {string} T
+   * @param {T} template
+   */
+  static mathML(template) {
+    return /** @type {Template<T, MathMLElementTagNameMap>} */ (
+      new Template(template, Template.#mathMLContext)
+    );
+  }
+
+  /**
+   * @template {string} T
+   * @param {T} template
+   */
+  static html(template) {
+    return new Template(template, document.documentElement);
+  }
 
   #fragment;
   /**
    * @param {T} template
+   * @param {M[keyof M]} contextNode
    */
-  constructor(template) {
+  constructor(template, contextNode) {
+    Template.#range.selectNodeContents(contextNode);
     this.#fragment = Template.#range.createContextualFragment(template);
   }
 
   /**
    * Clones the underlying markup with element references.
-   * @this {ExtractSlots<T> extends never ? Template<T> : never}
+   * @template {Record<ExtractSlots<T>, ChildNode | Markup<any, any>>} U
+   * @param {ExtractSlots<T> extends never ? [] : [slotted: U]} args
    */
-  bind() {
-    return this.bindWith(
-      /** @type {Record<ExtractSlots<T>, ChildNode>} */ ({}),
+  bind(...args) {
+    const slotted =
+      args[0] ?? /** @type {Record<ExtractSlots<T>, ChildNode>} */ ({});
+    return this.#bindWith(
+      /** @type {ExtractSlots<T> extends never ? Record<ExtractSlots<T>, ChildNode> : U} */ (
+        slotted
+      ),
     );
   }
 
@@ -55,7 +103,7 @@ export class Template {
    * @template {Record<ExtractSlots<T>, ChildNode | Markup<any, any>>} U
    * @param {U} slotted
    */
-  bindWith(slotted) {
+  #bindWith(slotted) {
     const fragment = /** @type {DocumentFragment} */ (
       this.#fragment.cloneNode(true)
     );
@@ -69,7 +117,7 @@ export class Template {
     const replaceNodes = [];
     for (let n = nodeIter.nextNode(); n; n = nodeIter.nextNode()) {
       if (n instanceof Element) {
-        const ref = n.getAttribute("data-ref");
+        const ref = n.getAttribute(Template.refAttr);
         if (ref != null) {
           refs[ref] = n;
         }
@@ -86,9 +134,17 @@ export class Template {
     for (const { node, slot } of replaceNodes) {
       slot.replaceWith(node instanceof Markup ? node.fragment : node);
     }
-    return new Markup(fragment, slotted, /** @type {ExtractRefs<T>} */ (refs));
+    return new Markup(
+      fragment,
+      slotted,
+      /** @type {ExtractRefs<T, M>} */ (refs),
+    );
   }
 }
+
+const a = Template.svg(/* html */ `<svg data-ref="s"><!--slot:t--></svg>`);
+const b = Template.html(/* html */ `<a data-ref="x"><!--slot:svg--></a>`);
+b.bind({ svg: a.bind({ t: new Text("abc") }) }).refs.x;
 
 /**
  * @template {string} S
@@ -108,20 +164,23 @@ export class Template {
  */
 /**
  * @template {string} S
- * @typedef {S extends `${string}<${infer TagAndAttr}>${infer Rest}` ?  (TagAndAttr extends MatchRefAndTag<infer T, infer Ref> ? { [K in Ref]: TagToElement<T> } & ExtractRefsHelper<Rest> : ExtractRefsHelper<Rest>) : {}} ExtractRefsHelper
+ * @template {{ [K in keyof M & string]: Element }} [M=HTMLElementTagNameMap]
+ * @typedef {S extends `${string}<${infer TagAndAttr}>${infer Rest}` ?  (TagAndAttr extends MatchRefAndTag<infer T, infer Ref> ? { [K in Ref]: TagToElement<T, M> } & ExtractRefsHelper<Rest, M> : ExtractRefsHelper<Rest, M>) : {}} ExtractRefsHelper
  */
 /**
  * @template {string} T
  * @template {string} R
- * @typedef {`${string} data-ref="${R}"${string}` & `${T} ${string}`} MatchRefAndTag
+ * @typedef {`${string} ${typeof Template['refAttr']}="${R}"${string}` & `${T} ${string}`} MatchRefAndTag
  */
 /**
  * @template {string} S
- * @typedef {ExtractRefsHelper<StripClosingTags<StripComments<S>>>} ExtractRefs
+ * @template {{ [K in keyof M & string]: Element }} [M=HTMLElementTagNameMap]
+ * @typedef {ExtractRefsHelper<StripClosingTags<StripComments<S>>, M>} ExtractRefs
  */
 /**
  * @template {string} K
- * @typedef {K extends keyof HTMLElementTagNameMap ? HTMLElementTagNameMap[K] : (K extends keyof SVGElementTagNameMap ? SVGElementTagNameMap[K] : (K extends keyof MathMLElementTagNameMap ? MathMLElementTagNameMap[K] : HTMLElement))} TagToElement
+ * @template {{ [K in keyof M & string]: Element }} [M=HTMLElementTagNameMap]
+ * @typedef {K extends keyof M ? M[K] : Element} TagToElement
  */
 /**
  * @template {string} S
